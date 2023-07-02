9362bd3854330f591dfa073ff6385e0d2f63d061
Miguel Arenas <migue.arenas91@gmail.com>
2023-07-01 20:37:40 -0500

    navBar responsive for Mobile done

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,39 @@ carouselItems.forEach(slide =>
   slide.addEventListener('transitionend', handleSlideTransition)
 )
 document.addEventListener('DOMContentLoaded', handleSlideTransition)
+
+// -----Modal Menu Mobile ----
+
+const mobileMenu = document.getElementById("mobileMenu_modal");
+console.log(mobileMenu);
+const hamburguerBtn = document.getElementById("menuModalBtn");
+console.log(hamburguerBtn);
+const closeModalBtn = document.getElementById("closeModalBtn");
+console.log(closeModalBtn);
+
+function openModalMenu() {
+  mobileMenu.style.display = "block";
+  setTimeout(() => {
+    mobileMenu.style.opacity = 1;
+  }, 10);
+}
+
+function closeModalMenu() {
+  mobileMenu.style.opacity = 0;
+  setTimeout(() => {
+    mobileMenu.style.display = "none";
+  }, 500);
+}
+
+window.onclick = function (e) {
+  if (e.target === mobileMenu) {
+    mobileMenu.style.opacity = 0;
+    setTimeout(() => {
+      mobileMenu.style.display = "none";
+    }, 500);
+  }
+};
+
+hamburguerBtn.addEventListener("click", openModalMenu);
+
+closeModalBtn.addEventListener("click", closeModalMenu);
