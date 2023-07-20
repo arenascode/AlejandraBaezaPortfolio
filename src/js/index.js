8879e8337280bffb2f5e6a75296f4a7bf5b597bb
Miguel Arenas <migue.arenas91@gmail.com>
2023-07-19 21:54:28 -0500

    featuredFilms in process

--- src/js/index.js
+++ src/js/index.js
@@ -192,8 +192,6 @@ function onYouTubeIframeAPIReady() {
       playsinline: 1,
     },
     events: {
-      // 'onReady': onPlayerReady,
-      // 'onStateChange': onPlayerStateChange,
     },
   });
 }
